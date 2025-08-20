503bfa928eeeecc132f5222f1bc058198e14fbc7
June <176018493+junapur@users.noreply.github.com>
2025-08-20 16:57:56 -0400

    feat: add DPI flags for the convert command

--- src/commands/convert.rs
+++ src/commands/convert.rs
@@ -1,32 +1,50 @@
 use crate::games::get_game;
-use crate::math::convert_sensitivity;
+use crate::math::{convert_sensitivity, convert_sensitivity_dpi};
 use crate::parsers::parse_positive_f64;
 use anyhow::{Result, anyhow};
-use clap::Args;
+use clap::{Args, value_parser};
 
 #[derive(Args)]
 pub struct ConvertArgs {
+    /// The game you're converting from
+    #[arg()]
+    from_game: String,
+
+    /// The game you're converting to
+    #[arg()]
+    to_game: String,
+
     /// Your in-game sensitivity
     #[arg(value_parser = parse_positive_f64)]
     sensitivity: f64,
 
-    /// The game you're converting from
-    #[arg()]
-    from: String,
+    /// Your current DPI
+    #[arg(requires = "to_dpi", value_parser = value_parser!(u32).range(1..))]
+    from_dpi: Option<u32>,
 
-    /// The game you're converting from
-    #[arg()]
-    to: String,
+    /// Your target DPI
+    #[arg(requires = "from_dpi", value_parser = value_parser!(u32).range(1..))]
+    to_dpi: Option<u32>,
 }
 
 pub fn execute(args: ConvertArgs) -> Result<()> {
-    let from_game =
-        get_game(&args.from).ok_or_else(|| anyhow!("Source game '{}' not found", args.from))?;
+    let from_game = get_game(&args.from_game)
+        .ok_or_else(|| anyhow!("Source game '{}' not found", &args.from_game))?;
 
-    let to_game =
-        get_game(&args.to).ok_or_else(|| anyhow!("Target game '{}' not found", args.to))?;
+    let to_game = get_game(&args.to_game)
+        .ok_or_else(|| anyhow!("Target game '{}' not found", &args.to_game))?;
 
-    let converted_sensitivity = convert_sensitivity(args.sensitivity, from_game.yaw, to_game.yaw);
+    let converted_sensitivity = match (args.from_dpi, args.to_dpi) {
+        (Some(from_dpi), Some(to_dpi)) => convert_sensitivity_dpi(
+            args.sensitivity,
+            from_game.yaw,
+            to_game.yaw,
+            from_dpi,
+            to_dpi,
+        ),
+        _ => convert_sensitivity(args.sensitivity, from_game.yaw, to_game.yaw),
+    };
+
     println!("{:.3}", converted_sensitivity);
 
     Ok(())
--- src/math.rs
+++ src/math.rs
@@ -2,6 +2,16 @@ pub fn get_cm_360(sensitvity: f64, dpi: u32, yaw: f64) -> f64 {
     (360.0 / (sensitvity * dpi as f64 * yaw)) * 2.54
 }
 
-pub fn convert_sensitivity(sensitvity: f64, from_yaw: f64, to_yaw: f64) -> f64 {
-    sensitvity * from_yaw / to_yaw
+pub fn convert_sensitivity(sensitivity: f64, from_yaw: f64, to_yaw: f64) -> f64 {
+    sensitivity * from_yaw / to_yaw
+}
+
+pub fn convert_sensitivity_dpi(
+    sensitivity: f64,
+    from_yaw: f64,
+    to_yaw: f64,
+    from_dpi: u32,
+    to_dpi: u32,
+) -> f64 {
+    sensitivity * from_yaw / to_yaw * (from_dpi as f64) / (to_dpi as f64)
 }
